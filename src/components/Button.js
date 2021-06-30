1b18c0e1f974483a5208e12ab1bb6bbba7bb03e7
ManassesM <manamiui2001@gmail.com>
2021-06-30 15:05:43 -0300

    Todo App ReactJS

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text, onClick }) => {
+  
+  return (
+    <button
+      style={{ backgroundColor: color }}
+      className="btn"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  color: 'Red',
+  text: 'btn',
+}
+
+Button.propTypes = {
+  color: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func
+}
+
+export default Button
